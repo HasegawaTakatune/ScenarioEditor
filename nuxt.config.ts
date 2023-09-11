dc02d099eff69f59fcb4de1a590b9b904dbe5de4
RerykA <0ta5274738t4p4r@gmail.com>
2023-09-11 19:37:47 +0900

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  app: {
+    baseURL: "/ScenarioEditor/",
+  },
+  devtools: { enabled: true },
+});
