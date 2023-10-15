2f391c627bf506c16c9cc6987578dcde5a837ccb
RerykA <0ta5274738t4p4r@gmail.com>
2023-10-15 13:55:48 +0900

    deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,28 @@
+const getBaseUrl = () => {
+  console.log("------------------------");
+  console.log(`NODE_ENV >> ${process.env.NODE_ENV}`);
+  console.log({ ...process.env });
+  console.log("------------------------");
+
+  const environment = process.env.NODE_ENV;
+
+  switch (environment) {
+    case "development":
+      return "/ScenarioEditor/";
+
+    case "staging":
+    case "production":
+      return "/https://hasegawatakatune.github.io/ScenarioEditor";
+    default:
+      return "/";
+  }
+};
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    baseURL: "/ScenarioEditor/",
+    baseURL: getBaseUrl(),
+    // baseURL: "/ScenarioEditor/",
   },
   devtools: { enabled: true },
 });
